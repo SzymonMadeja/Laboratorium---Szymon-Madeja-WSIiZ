c09bef32ce0c9c12aa4a54be8e1469a3198383e8
Szymon Madeja <108612900+SzymonMadeja@users.noreply.github.com>
2025-10-24 14:24:56 +0200

    Add files via upload

---
+++ zadanie_1a.py
@@ -0,0 +1,9 @@
+print("1. ",type(1 + 2)) # + Operator dodawania
+print("2. ",type(1 + 4.5)) # + Operator dodawania
+print("3. ",type(3/2)) # / Operator dzielenia z resztą
+print("4. ",type(4/2)) # / Operator dzielenia z resztą
+print("5. ",type(3//2)) # / Operator dzielenia całkowitego (bez reszty, nawet jeśli istnieje)
+print("6. ",type(-3//2)) # / Operator dzielenia całkowitego (bez reszty, nawet jeśli istnieje)
+print("7. ",type(11 % 2)) # % Operator modulo - Wypisanie tylko reszty z dzielenia podanych liczb
+print("8. ",type(2 ** 10)) # ** Operator potęgowania
+print("9. ",type(8 ** (1/3))) # ** Operator potęgowania, / Operator potęgowania z resztą a () oznacza, że najpierw wykona się rownanie w nich zawarte a pozniej dopiero reszta
